cb51f5aaf9542491e42c18d28a2e1948d6fa34c8
krzysztof.janosz <kjanosz62@gmail.com>
2021-04-21 11:58:38 +0200

    fix for paths #3

--- src/views/footer/footer.js
+++ src/views/footer/footer.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
     $("a.footer-arrow").click(function() {
         if($("#main-content").find("#home-container").length !== 0){
-            $("#main-content").load("./views/home/home.html");
+            $("#main-content").load("./src/views/home/home.html");
         }
         else if($("#main-content").find("#about-container").length !== 0){
-            $("#main-content").load("./views/about/about.html");
+            $("#main-content").load("./src/views/about/about.html");
         }
         else if($("#main-content").find("#skills-container").length !== 0){
-            $("#main-content").load("./views/skills/skills.html");
+            $("#main-content").load("./src/views/skills/skills.html");
         }
         else if($("#main-content").find("#projects-container").length !== 0){
-            $("#main-content").load("./views/projects/projects.html");
+            $("#main-content").load("./src/views/projects/projects.html");
         }
         else if($("#main-content").find("#contact-container").length !== 0){
-            $("#main-content").load("./views/contact/contact.html");
+            $("#main-content").load("./src/views/contact/contact.html");
         }
     });
 });
--- src/views/navigation-menu/navigationMenu.js
+++ src/views/navigation-menu/navigationMenu.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
     $(".logo-container .logo").click(function() {
-        $("#main-content").load("./views/home/home.html");
+        $("#main-content").load("./src/views/home/home.html");
     });
 
     $(".home").click(function() {
-        $("#main-content").load("./views/home/home.html");
+        $("#main-content").load("./src/views/home/home.html");
         $(".menu-btn").removeClass("close");
         $(".menu-dropdown").removeClass("show");
         $("nav.nav-top .field-transparent").removeClass("show");
     });
 
     $(".about").click(function() {
-        $("#main-content").load("./views/about/about.html");
+        $("#main-content").load("./src/views/about/about.html");
         $(".menu-btn").removeClass("close");
         $(".menu-dropdown").removeClass("show");
         $("nav.nav-top .field-transparent").removeClass("show");
     });
 
     $(".skills").click(function() {
-        $("#main-content").load("./views/skills/skills.html");
+        $("#main-content").load("./src/views/skills/skills.html");
         $(".menu-btn").removeClass("close");
         $(".menu-dropdown").removeClass("show");
         $("nav.nav-top .field-transparent").removeClass("show");
     });
 
     $(".projects").click(function() {
-        $("#main-content").load("./views/projects/projects.html");
+        $("#main-content").load("./src/views/projects/projects.html");
         $(".menu-btn").removeClass("close");
         $(".menu-dropdown").removeClass("show");
         $("nav.nav-top .field-transparent").removeClass("show");
     });
 
     $(".contact").click(function() {
-        $("#main-content").load("./views/contact/contact.html");
+        $("#main-content").load("./src/views/contact/contact.html");
         $(".menu-btn").removeClass("close");
         $(".menu-dropdown").removeClass("show");
         $("nav.nav-top .field-transparent").removeClass("show");
